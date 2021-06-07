b69ed512b7ae5b7b592fbaf115fbb5af77083069
Mark Daquis <36392970+markzero27@users.noreply.github.com>
2021-06-07 18:42:08 +0800

    Initial Commit

---
+++ H4Q3R/H4Q3RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4Q3RApp.swift
+//  H4Q3R
+//
+//  Created by Marcus Daquis on 6/7/21.
+//
+
+import SwiftUI
+
+@main
+struct H4Q3RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
